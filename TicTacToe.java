5216365fb0dd9e91e2c97f60cf6191913e20518b
michelleas <marmstr2@macalester.edu>
2018-03-24 20:14:06 -0500

    hellooooo again

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,2 +1,34 @@
-public class TicTacToe {
+import comp124graphics.CanvasWindow;
+
+public class TicTacToe extends CanvasWindow {
+
+    private static final int CANVAS_WIDTH = 800;
+    public static final int CANVAS_HEIGHT = 900;
+    private int numX, numO;
+
+    public TicTacToe(String title, int width, int height){
+        super(title, width, height);
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe("Tic Tac Toe", CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+
+    public boolean checkCat(){
+        return numX == numO;
+    }
+
+    public char checkWin(){
+        if(!checkCat()) {
+            if (numX > numO)
+                return 'X';
+            else
+                return 'O';
+        }
+        return 'C';
+    }
+
+    public void newGame(){
+
+    }
 }
