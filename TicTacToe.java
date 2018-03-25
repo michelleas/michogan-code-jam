fc4ed3890f4027c4ad4926180e0a43142f3eaa28
michelleas <marmstr2@macalester.edu>
2018-03-24 21:30:23 -0500

    progress?

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,24 @@
 import comp124graphics.CanvasWindow;
+import comp124graphics.GraphicsText;
+import comp124graphics.Rectangle;
 
-public class TicTacToe extends CanvasWindow {
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class TicTacToe extends CanvasWindow implements MouseListener, MouseMotionListener{
 
     private static final int CANVAS_WIDTH = 800;
     public static final int CANVAS_HEIGHT = 900;
+
+    private Rectangle button;
+    private GraphicsText text;
+    private Square square;
+
     private int numX, numO;
+    private char mark;
+    private char[] turn = {'X', 'O'};
 
     public TicTacToe(String title, int width, int height){
         super(title, width, height);
@@ -12,6 +26,21 @@ public class TicTacToe extends CanvasWindow {
 
     public static void main(String[] args) {
         TicTacToe game = new TicTacToe("Tic Tac Toe", CANVAS_WIDTH, CANVAS_HEIGHT);
+        game.newGame();
+    }
+
+    public void newGame(){
+        for(int i = 0; i < 9; i++) {
+            if (turn[i] == 'X') {
+                mark = 'X';
+                numX++;
+            }
+            if (turn[i] == 'O') {
+                mark = 'O';
+                numO++;
+            }
+        }
+        newGameButton();
     }
 
     public boolean checkCat(){
@@ -28,7 +57,53 @@ public class TicTacToe extends CanvasWindow {
         return 'C';
     }
 
-    public void newGame(){
+    public void newGameButton(){
+        button = new Rectangle(710, 10, 80, 30);
+        button.setFillColor(Color.cyan);
+        button.setFilled(true);
 
+        text = new GraphicsText("New Game", 715, 30);
+
+        add(button);
+        add(text);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(e.getX() >= button.getX() && e.getY() <= button.getY()) {
+            System.out.println("clicked");
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        if(e.getX() <= square.getX())
+            square.highlight(true);
+        if(square.canMark())
+            square.makeMark(mark);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
     }
 }
