9182fd84814e1a8d835fe369bd4d7670f81cd72b
SybelBlue <lbc.dna@gmail.com>
2018-03-24 22:20:43 -0500

    Finished Gridlines hellllo sweetheart

--- GridLines.java
+++ GridLines.java
@@ -1,11 +1,33 @@
 import comp124graphics.GraphicsGroup;
+import comp124graphics.Rectangle;
 
 public class GridLines extends GraphicsGroup {
 
-    public static final double LINE_WIDTH = TicTacToe.CANVAS_WIDTH / 15.0;
-    public static final double LINE_LENGTH = 13 * LINE_WIDTH;
+    public static final double LINE_WIDTH = TicTacToe.MARGIN;
+    public static final double LINE_HEIGHT = 13 * LINE_WIDTH;
 
-    public GridLines(double x, double y) {
-        super(x, y);
+    public GridLines() {
+        super(0, 0);
+
+        double spacing = Square.SIDE_LENGTH + TicTacToe.MARGIN;
+
+        add(makeGridline(spacing, TicTacToe.MARGIN));
+        add(makeGridline(2 * spacing, TicTacToe.MARGIN));
+        add(makeGridline(TicTacToe.MARGIN, spacing));
+        add(makeGridline(TicTacToe.MARGIN, 2 * spacing));
+    }
+
+    private Rectangle makeGridline(double x, double y) {
+        Rectangle line;
+
+        if (x > y)
+            line = new Rectangle(x, y, LINE_WIDTH, LINE_HEIGHT);
+        else
+            line = new Rectangle(x, y, LINE_HEIGHT, LINE_WIDTH);
+
+        line.setFilled(true);
+        line.setFillColor(java.awt.Color.black);
+
+        return line;
     }
 }
